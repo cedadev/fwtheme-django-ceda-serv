d5977fac5c50ab2f4803b086a2724509ab221d34
dwest77a <daniel.westwood@stfc.ac.uk>
2023-06-16 09:20:56 +0100

    Added beacon support

--- fwtheme_django_ceda_serv/context_processors.py
+++ fwtheme_django_ceda_serv/context_processors.py
@@ -1,24 +1,27 @@
-from .default_settings import DC_TEMPLATES, DATACENTRES
+from .default_settings import DC_TEMPLATES, DATACENTRES, BEACONS
 
-
-def data_centre(request):
+def data_centre(request, arrivals=False):
     """
     Determine which data centre template is extended
     by browse.html
     """
-
     location = 'extra_data_centres/'
     data_centre_template = 'fwtheme_django/layout.html'  # default template - NERC EDS Theme
 
     # fwtheme_django_ceda_serv version 1.5.3
 
     # Dynamic Data Centre processing
+    beacon = BEACONS['CEDA']
     dc = request.path.split('/')[1] # /ceda/ -> ['','ceda','...']
     if dc not in ['ceda','badc','neodc'] and dc in DATACENTRES:
         data_centre_template = location + DC_TEMPLATES[dc]
-        
+        beacon = BEACONS['EDS']
+    print('Req',request)
+    if arrivals:
+        beacon = BEACONS['Arrivals']    
     context = {
         "data_centre_template": data_centre_template,
-        "data_centre": dc
+        "data_centre": dc,
+        "beacon":beacon,
     }
     return context
--- fwtheme_django_ceda_serv/default_settings.py
+++ fwtheme_django_ceda_serv/default_settings.py
@@ -32,6 +32,12 @@ DC_TEMPLATES = {
     'sparc': 'sparc.html'
 }
 
+BEACONS = {
+    'CEDA':'0151e3db-1a06-48d6-a030-d1c605683be9',
+    'EDS':'9d36e853-6905-459c-bbed-d62ec83150e5',
+    'Arrivals':'747016d7-b078-464f-89b0-1dbe00d653bc',
+}
+
 # Enable the user_status block for Django-authenticated users
 USE_DJANGO_USER_STATUS = False
 
